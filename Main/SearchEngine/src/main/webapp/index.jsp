5bddf19d4875118cdd1adb84bd711911c442640b
Alok290 <79790013+Alok290@users.noreply.github.com>
2023-08-23 00:05:55 +0530

    Add files via upload

---
+++ Main/SearchEngine/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+    <link rel ="Stylesheet" type = "text/css" href = "styles.css">
+
+</head>
+<body>
+<h1>Search Engine using DFS</h1>
+<form action= "Search">
+<input type = "text" name = "keyword"></input>
+<button type = "submit">Search</button>
+</form>
+
+<form action= "History">
+<button type = "submit">History</button>
+</form>
+
+</body>
+</html>
---
+++ Main/webCrawler/src/main/java/org/example/DBConnection.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    static Connection connection = null;
+
+     public static Connection getConnection()
+     {
+         if(connection!=null) return connection;
+
+         String user = "root";
+         String pwd = "Alok123@";
+         String db = "db";
+         return getConnection(user,pwd,db);
+
+     }
+     private static Connection getConnection(String user, String pwd, String db){
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/" + db + "?user=" + user + "&password=" + pwd);
+    }
+     catch(
+    SQLException sqlException)
+    {
+        sqlException.printStackTrace();
+    }
+     catch(ClassNotFoundException classNotFoundException)
+    {
+        classNotFoundException.printStackTrace();
+    }
+     return connection;
+}
+
+}
